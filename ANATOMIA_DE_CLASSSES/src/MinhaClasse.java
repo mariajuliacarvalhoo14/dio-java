f44493e0c16d61afe5886c5df9f58db6a8882242
mariajuliacarvalhoo14 <~161620231069@faculdadedamas.edu.br>
2024-03-25 15:55:23 -0300

    versao atualizada com a aula anatomia de classes 2

--- ANATOMIA_DE_CLASSSES/src/MinhaClasse.java
+++ ANATOMIA_DE_CLASSSES/src/MinhaClasse.java
@@ -1,7 +1,14 @@
 public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Maria julia";
+        String segundoNome = "Carvalho";
+         String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+         System.out.println(nomeCompleto);
+    }
 
-   
-   
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
     }
     
     
